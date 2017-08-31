851638d9f1c5bcbd660937b8e85dcd55ed9e6f78
jbcoppola <jbcoppola@gmail.com>
2017-08-31 08:42:41 -0700

    inital commit

---
+++ app/main.js
@@ -0,0 +1,22 @@
+﻿//make local model
+var app = angular.module("Company", []);
+
+//make controller
+app.controller("Company", function ($scope) {
+    $scope.items = [];
+
+    //create new company
+    $scope.addItem = function (item) {
+        $scope.items.push(item);
+        $scope.item = {};
+    },
+    //delete company
+    $scope.removeItem = function (index) {
+        console.log(index);
+        $scope.items.splice(index, 1)
+    },
+    //update company
+    $scope.editItem = function (index) {
+        $scope.editing = $scope.items.indexOf(index);
+    }
+});
