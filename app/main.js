e3b723c1c0a64937e60709e0e6e5a685f3751415
jbcoppola <jbcoppola@gmail.com>
2017-08-31 13:22:54 -0700

    added company list

--- app/main.js
+++ app/main.js
@@ -3,7 +3,28 @@ var app = angular.module("Company", []);
 
 //make controller
 app.controller("Company", function ($scope) {
-    $scope.items = [];
+    //populate with company data
+    $scope.items = 
+        [
+        {
+            "company": "Google",
+            "status": "Aquired",
+            "information": "Search Engine",
+            "contacts": "Alan Smithee",
+            "performance": "100"
+        }
+        ]
+
+    //populate with json company data
+    /*
+    $http.get('data/companyList.json').then(successCallback, errorCallback);
+    function successCallback(response) {
+        $scope.items = response.data;
+    }
+    function errorCallback(error) {
+        $scope.items = [];
+    }
+    */
 
     //create new company
     $scope.addItem = function (item) {
@@ -19,4 +40,6 @@ app.controller("Company", function ($scope) {
     $scope.editItem = function (index) {
         $scope.editing = $scope.items.indexOf(index);
     }
+
+
 });
